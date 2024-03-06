68155f92d6b575e4819dc18bf55145e64ffa503a
최혜림 <chhari0708@naver.com>
2024-03-06 19:24:44 +0900

    add router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
+import {useNavigate} from "react-router-dom";
 import "../styles/header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
   const isLogin = false; // 로그인 되어있는 헤더 확인 시 true 변경 후 확인
+
   return (
     <div className="header">
-      <div className="logo">LOGO</div>
+      <div
+        className="logo"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        LOGO
+      </div>
       <div className="menu">
         {isLogin ? (
           <div className="logout">로그아웃</div>
--- src/pages/Creatediary.js
+++ src/pages/Creatediary.js
@@ -1,39 +1,43 @@
+import Header from "../components/Header";
 import "../styles/create-diary.css";
 
 const Creatediary = () => {
   return (
-    <div className="create-diary">
-      <div className="diary-form">
-        <div className="dateInput">
-          <label>날짜</label>
-          <div className="date">2024.03.06</div>
-        </div>
-        <div className="title-weather">
-          <div className="title">
-            <label>제목</label>
-            <input
-              className="title-input"
-              type="text"
-              placeholder="제목을 입력해주세요."
-            />
+    <>
+      <Header />
+      <div className="create-diary">
+        <div className="diary-form">
+          <div className="dateInput">
+            <label>날짜</label>
+            <div className="date">2024.03.06</div>
           </div>
-          <div className="weather">
-            <label>날씨</label>
-            <input className="weather-input" type="text" placeholder="날씨" />
+          <div className="title-weather">
+            <div className="title">
+              <label>제목</label>
+              <input
+                className="title-input"
+                type="text"
+                placeholder="제목을 입력해주세요."
+              />
+            </div>
+            <div className="weather">
+              <label>날씨</label>
+              <input className="weather-input" type="text" placeholder="날씨" />
+            </div>
           </div>
-        </div>
-        <div className="content">
-          <label>내용</label>
-          <textarea
-            className="content-input"
-            placeholder="내용을 입력해주세요."
-          ></textarea>
-        </div>
-        <div className="btnWrap">
-          <button className="diarySubmitBtn">작성완료</button>
+          <div className="content">
+            <label>내용</label>
+            <textarea
+              className="content-input"
+              placeholder="내용을 입력해주세요."
+            ></textarea>
+          </div>
+          <div className="btnWrap">
+            <button className="diarySubmitBtn">작성완료</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
