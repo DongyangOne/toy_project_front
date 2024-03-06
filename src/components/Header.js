a55bbf759fe2aa6ba025a78d2518725df039acac
최혜림 <chhari0708@naver.com>
2024-03-06 15:46:33 +0900

    login header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import "../styles/header.css";
 
 const Header = () => {
+  const isLogin = false; // 로그인 되어있는 헤더 확인 시 true 변경 후 확인
   return (
     <div className="header">
       <div className="logo">LOGO</div>
       <div className="menu">
-        <div className="login">로그인</div>
-        <div className="register">회원가입</div>
+        {isLogin ? (
+          <div className="logout">로그아웃</div>
+        ) : (
+          <>
+            <div className="login">로그인</div>
+            <div className="register">회원가입</div>
+          </>
+        )}
       </div>
     </div>
   );
